fc183d656f7b5a1cd2e03dc655052dffe88e1706
rodaveli <rodrigoabreu56@gmail.com>
2024-08-29 17:07:54 -0500

    tgtgtgtg

--- database.py
+++ database.py
@@ -5,6 +5,9 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 if DATABASE_URL:
     engine = create_engine(DATABASE_URL)
 else:
